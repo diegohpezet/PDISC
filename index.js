9d594f65a75dfd8d10783ff53eb56b6da4c3d345
ailin <ailinantonellasalas05@gmail.com>
2025-06-27 18:16:58 -0300

    Agregado de Express.Js junto a posterior prueba en index.js, correccion de package.json (agregado de type: module

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Ailin Salas - 7*4 PDISC") 
+// Servidor básico con Express
+import express from 'express';  // Importamos express
+
+const app = express();  // Creamos una instancia de express
+
+app.get('/', function (req, res) {  // Creamos una 'ruta' de ejemplo dentro del servidor
+  res.send('¡Hola mundo! (pero con Express)');
+});
+
+app.listen(3000, () => {  // Iniciamos el servidor
+  console.log('Servidor corriendo en http://localhost:3000');
+});
